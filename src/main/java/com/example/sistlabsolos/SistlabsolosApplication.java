0c4cdcd1f84bfb9ae559385c2e533f028c2c50f7
LeonardoFariaOliveira <leonardoN00Bfaria@hotmail.com>
2023-12-20 14:41:23 -0300

    Set repository

---
+++ src/main/java/com/example/sistlabsolos/SistlabsolosApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistlabsolos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistlabsolosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistlabsolosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistlabsolos/SistlabsolosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistlabsolos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistlabsolosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
