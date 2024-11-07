47763d0c4565d7c204844290da4b78fb343763c2
LeonardoFariaOliveira <leonardoN00Bfaria@hotmail.com>
2024-11-07 18:06:30 -0300

    fixing cors

--- src/main/java/com/example/sistlabsolos/config/CorsConfig.java
+++ src/main/java/com/example/sistlabsolos/config/CorsConfig.java
@@ -16,8 +16,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
             }
         };
     }
